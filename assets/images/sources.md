d7413f0e3a96e893eeb465329dc7e6bdd51cf1da
SergenKaraoglan <sergenkaraoglan@outlook.com>
2021-08-18 15:26:32 +0100

    images and panel js

---
+++ assets/images/sources.md
@@ -0,0 +1,3 @@
+"File:Trex pixel.png" by Martingggg is licensed with CC BY 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by/4.0
+
+https://fontawesome.com/
---
+++ assets/js/panel.js
@@ -0,0 +1,4 @@
+if (window.screen.width <= 428){
+    console.log(document.getElementsByClassName("wrapper")[0].style);
+    document.getElementsByClassName("wrapper")[0].style.gridTemplateColumns = "repeat(1, 1fr)";
+}
