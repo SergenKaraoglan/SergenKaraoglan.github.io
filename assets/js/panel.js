f6c3e3982cd429d310eba0a798550076c50665c7
SergenKaraoglan <sergenkaraoglan@outlook.com>
2021-08-23 20:07:04 +0100

    align items

--- assets/js/panel.js
+++ assets/js/panel.js
@@ -1,3 +1,10 @@
 if (window.screen.width <= 428){
     document.documentElement.style.setProperty("--coloumn_count","repeat(1)");
 }
+
+function resizeGrid(){
+    if (window.innerWidth < 600){
+        document.getElementsByClassName("wrapper")[0].style.gridTemplateColumns = "repeat(1, 1fr)";
+    }
+}
+window.onresize = resizeGrid;
